518765cacec8c54932ab46488ea751596c9a0152
pompeypigeon <pompeypigeon@gmail.com>
2018-01-30 13:55:12 +0000

    Delete serv2er.js

--- serv2er.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-var app = require('./app'),
-    db = require('./db'),
-    port = process.env.PORT || 8000,
-    server = app.listen(port, function(){
-      console.log("server started on " + port)
-    });
