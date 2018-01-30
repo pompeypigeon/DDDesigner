26e82a10e4cef558b879bdaf2bfbc3587e8d0985
pompeypigeon <pompeypigeon@gmail.com>
2018-01-30 13:54:56 +0000

    New

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
-const 	url = require('url')
-				express = require('express'),
-				app = express(),
-				server = require('http').Server(app),
-				io = require('socket.io').listen(server),
-				port = process.env.PORT || 8080;
-
-server.listen(port, function(){
-	console.log("on port "+ port);
-})
-
-console.log("Server running!");
-
-app.use(express.static(__dirname + '/static'))
-
-app.get('/dictionary', function(req,res){
-	var something = "HVYB(S*b89";
-	return something;
-})
-
-app.get('/', function(req,res){
-	res.sendFile(__dirname, '/static/index.html');
-})
+'use strict'
+var app = require('./app'),
+    db = require('./db'),
+    port = process.env.PORT || 8000,
+    server = app.listen(port, function(){
+      console.log("server started on " + port)
+});
