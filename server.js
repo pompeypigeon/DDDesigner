ae926a417f36c63e59d49a7e37b0739b2031467a
pompeypigeon <pompeypigeon@gmail.com>
2018-02-06 15:26:43 +0000

    Added db connection and tidied up

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
-<<<<<<< HEAD
-const 	url = require('url')
-		express = require('express'),
-		app = express(),
-		server = require('http').Server(app),
-		io = require('socket.io').listen(server),
-		port = process.env.PORT || 8080;
+'use strict'
+//server
+const	express = require('express'),
+			app = express(),
+			server = require('http').Server(app),
+			io = require('socket.io').listen(server),
+			port = 8000;
+
+//database
+const mysql = require('mysql'),
+			conn = mysql.createConnection({
+				host: 'localhost',
+				user: 'root',
+				password: 'root',
+				database: 'datadictionary'
+			});
 
 server.listen(port, function(){
-	console.log("on port "+ port);
-})
-console.log("Server running!");
-
-app.use(express.static(__dirname + '/pages'))
-
-app.get('/dictionary', function(req,res){
-	var something = "HVYB(S*b89";
-	res.send(something);
-})
-
-
-app.get('/', function(req,res){
-	res.sendFile(__dirname, '/pages/index.html');
-})
-=======
-'use strict'
-var app = require('./app'),
-    db = require('./db'),
-    port = process.env.PORT || 8000,
-    server = app.listen(port, function(){
-      console.log("server started on " + port)
+	console.log('Running on '+ port);
 });
->>>>>>> 518765cacec8c54932ab46488ea751596c9a0152
+
+conn.connect(function(err){
+	if(err){
+		console.log('No connecttion', err.errorno, err.fatal);
+		return;
+	}
+	console.log('Connection to database established');
+})
+
+app.use(express.static(__dirname + '/static'));
